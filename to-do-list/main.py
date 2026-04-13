162215bf75c46abe9b2f5cc006cd61dc5d7e766f
Caio Cotrim <caio.cotrimps4@gmail.com>
2026-04-13 17:38:27 -0300

    criacao repositorio e add to-do-list

---
+++ to-do-list/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+tasks = []
+
+class Task(BaseModel):
+    id: int
+    titulo: str
+    completa: bool = False
+
+@app.post("/tasks")
+def criar_task(nova_task: Task):
+    for task in tasks:
+        if nova_task.id == task.id:   
+            return {"erro": "ID inserido já está sendo utilizado"}
+        if nova_task.titulo == task.titulo:
+            return {"erro": "Título inserido já está sendo utilizado"}
+            
+    tasks.append(nova_task)    
+    return nova_task
+
+@app.get("/tasks")
+def visualizar_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def buscar_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    return {"erro": "Tarefa não encontrada"}
+
+@app.delete("/tasks/{task_id}")
+def deletar_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return {"mensagem": f"Tarefa {task_id} apagada"}
+    return {"erro": "Tarefa não encontrada"} 
+
+@app.put("/tasks/{task_id}")
+def alterar_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            task.completa = True
+            return {"mensagem": f"Tarefa {task_id} concluída"}
+    return {"mensagem": "Tarefa não encontrada"} 
